bcae28d6b464aa681bdaa5119afa56d7b5667e1a
pennyqxr <pennyqxr@gmail.com>
2021-11-04 12:24:21 +0100

    expose the service to AKS

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# azure-prbot
+Simple Python Flask Dockerized Application#
+Build the image using the following commands:
+
+
+`docker build -t simple-flask-app:latest .`
+
+Run the Docker container using the command shown below.
+
+
+`docker run -d -p 5001:5001 simple-flask-app`
+
+The application will be accessible at http:127.0.0.1:5001 or if you are using boot2docker then first find ip address using $ boot2docker ip and the use the ip http://<host_ip>:5001
+
+
+# TODOs:
+- how to store the pat into a secret that can be fetched in a container
+- how to see python application logs in a container
--- app.py
+++ app.py
@@ -2,11 +2,16 @@ from flask import Flask
 import os, json
 import requests
 import base64
+import logging
 
-def get_pipeline_run_results(pipeline_id=15872, run_id=1112141):
-    base_url = "https://dev.azure.com/cbsp-abnamro/MAAP2/_apis/pipelines/{}/runs/{}?api-version=6.0-preview.1"
+logger = logging.getLogger("prbot")
+PAT = "p5l5wqllrljcmbhzaq4cs3ejmdl7nbs4mtqrlnmyjqydwsn2gwta"
+
+
+def get_pipeline_run_results(pipeline_id=1, run_id=48):
+    base_url = "https://dev.azure.com/pennyqxr/TestMLOpsPython/_apis/pipelines/{}/runs/{}?api-version=6.0-preview.1"
     username = "" # This can be an arbitrary value or you can just let it empty
-    password = "eecmbibcnl3r65izksvqsed75kxvzjj6w6lyod6jvmdjvskyrjxa"
+    password = PAT
     userpass = username + ":" + password
     b64 = base64.b64encode(userpass.encode()).decode()
 
@@ -14,16 +19,23 @@ def get_pipeline_run_results(pipeline_id=15872, run_id=1112141):
 
     final_url = base_url.format(pipeline_id, run_id)
     response_get = requests.get(final_url, headers=headers).json()
-    comment_payload = {
-        'status': response_get['state'],
-        'result': response_get['result'],
+    try:
+        comment_payload = {
+            'status': response_get['state'],
+            'result': response_get['result'],
+        }
+    except KeyError as e:
+        print(e)
+        comment_payload = {
+            'message': response_get['message']
     }
     return comment_payload
 
+
 def post_comment_on_pr(pr_id, comment):
-    base_url = "https://dev.azure.com/cbsp-abnamro/MAAP2/_apis/git/repositories/mlops-repo-actions-bot/pullRequests/{}/threads/?api-version=6.0"
+    base_url = "https://dev.azure.com/pennyqxr/TestMLOpsPython/_apis/git/repositories/MMM/pullRequests/{}/threads/?api-version=6.0"
     username = "" # This can be an arbitrary value or you can just let it empty
-    password = "eecmbibcnl3r65izksvqsed75kxvzjj6w6lyod6jvmdjvskyrjxa"
+    password = PAT
     userpass = username + ":" + password
     b64 = base64.b64encode(userpass.encode()).decode()
 
@@ -33,7 +45,7 @@ def post_comment_on_pr(pr_id, comment):
       "comments": [
         {
           "parentCommentId": 0,
-          "content": comment,
+          "content": "whatever for test",
           "commentType": 1
         }
       ],
@@ -43,37 +55,39 @@ def post_comment_on_pr(pr_id, comment):
     final_url = base_url.format(pr_id)
     response = requests.post(final_url, data=json.dumps(payload), headers=headers)
 
-    return response
+    return response.status_code
+
 
 def trigger_pipeline(repo_endpoint_url):
     repo_endpoint_url = repo_endpoint_url
 
     username = "" # This can be an arbitrary value or you can just let it empty
     # TODO: how to encrypt this password?
-    password = "eecmbibcnl3r65izksvqsed75kxvzjj6w6lyod6jvmdjvskyrjxa"
+    password = PAT
     userpass = username + ":" + password
     b64 = base64.b64encode(userpass.encode()).decode()
     headers = {"Authorization" : "Basic %s" % b64, 'Content-Type': 'application/json'}
 
     response_get = requests.get(repo_endpoint_url, headers=headers)
-    print("list pipeline:", response_get.status_code) # Expect 200
+    logger.info("list pipeline:", response_get.status_code) # Expect 200
 
     data={
         "resources": {
             "repositories": {
                 "self": {
-                    "refName": "refs/heads/master"
+                    "refName": "refs/heads/main"
                 }
             }
         }
     }
     response_post = requests.post(repo_endpoint_url, headers=headers, data=json.dumps(data))
-    print("run pipeline:", response_post.status_code)
-    print(response_post.content)
+    logger.info("run pipeline:", response_post.status_code)
+    logger.info(response_post.content)
     return response_post.status_code
 
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def hello():
@@ -89,19 +103,21 @@ def event_handler():
     # ref: https://docs.microsoft.com/en-us/azure/devops/service-hooks/events?view=azure-devops#work-item-commented-on
     with open('commented_on_event.json') as f:
         event = json.load(f)
-    print(event)
+    logger.info("event is ", event)
 
     if "/run-full-test" in event['detailedMessage']['text']:
         # we trigger the matched pipeline here using PR api https://docs.microsoft.com/en-us/rest/api/azure/devops/pipelines/runs/run-pipeline?view=azure-devops-rest-6.0
-        status_code = trigger_pipeline("https://dev.azure.com/cbsp-abnamro/MAAP2/_apis/pipelines/15872/runs?api-version=6.0-preview.1")
-        if status_code == '200':
+        status_code = trigger_pipeline("https://dev.azure.com/pennyqxr/TestMLOpsPython/_apis/pipelines/1/runs?api-version=6.0-preview.1")
+        if str(status_code) == '200':
             # we post the status to PR thread as a comment
-            # TODO
-            comment = get_pipeline_run_results()
-            post_comment_on_pr(95016, comment) # replace the PR ID with organic ID
+            logger.info("get 200 status for trigger pipeline")
+            comment = get_pipeline_run_results(pipeline_id=1, run_id=48)
+            post_comment_on_pr(pr_id=9, comment=comment) # replace the PR ID with organic ID
     return "Events handled"
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5001))
+    port = int(os.environ.get("PORT", 8080))
     app.run(debug=True, host='0.0.0.0', port=port)
+
+
